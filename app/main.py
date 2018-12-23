cb7063aa63b91467c75fefed14be066e6a560756
crt68 <43398160+crt68@users.noreply.github.com>
2018-12-23 00:27:44 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -23,14 +23,7 @@ airport_util = Airports()
 
 @app.route('/airportName', methods=['GET'])
 def airportName():
-    """Given an airport IATA code, return that airport's name."""
-    iata_code = request.args.get('iataCode')
-    if iata_code is None:
-      return 'No IATA code provided.', 400
-    maybe_name = airport_util.get_airport_by_iata(iata_code)
-    if maybe_name is None:
-      return 'IATA code not found : %s' % iata_code, 400
-    return maybe_name, 200
+    return 'Hello'
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
